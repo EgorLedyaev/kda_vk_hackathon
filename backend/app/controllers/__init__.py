f97992dbcf2435e257c3c5052d13940921cadfbb
Dmitry Ermilov <aiker1995@yandex.ru>
2022-09-24 00:35:36 +0300

    Add Model & Get Json Front

---
+++ backend/app/controllers/__init__.py
@@ -0,0 +1,1 @@
+from .LoaderController import LoaderController
--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -1,2 +1,16 @@
 
 require("./bootstrap.js")
+var fileobj;
+function upload_file(e) {
+    e.preventDefault();
+    fileobj = e.dataTransfer.files[0];
+    ajax_file_upload(fileobj);
+}
+    
+function file_explorer() {
+    document.getElementById('selectfile').click();
+    document.getElementById('selectfile').onchange = function() {
+        fileobj = document.getElementById('selectfile').files[0];
+        ajax_file_upload(fileobj);
+    };
+}
